367ea0358d510c19e7d25a553628acceb903c9af
Rafael <rafgonz1222@gmail.com>
2024-01-02 13:38:13 -0600

    update code

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
-let inputBtn = document.getElementById("input-btn")
+const inputBtn = document.getElementById("input-btn")
+const inputEl = document.getElementById("input-el")
+const ulEl = document.getElementById("ul-el")
+let myLeads = []
 
 inputBtn.addEventListener("click", function() {
-    console.log("Button clicked!")
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    renderLeads()
 })
+
+function renderLeads() {    
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems += `
+            <li>
+              <a href="${myLeads[i]}" target="_blank">
+                ${myLeads[i]}
+              </a>
+            </li>
+        `
+    }
+    ulEl.innerHTML = listItems
+}
