f7d9584346f0440535c8c3f61bb779e91841272d
Rafael <rafgonz1222@gmail.com>
2024-01-02 17:14:48 -0600

    updated code

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ let myLeads = []
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
     renderLeads()
+    console.log(localStorage.getItem("myLeads"))
 })
 
 function renderLeads() {    
